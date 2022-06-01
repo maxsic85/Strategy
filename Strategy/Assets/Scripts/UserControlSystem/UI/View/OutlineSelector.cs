d65f557f64d92bf53352954ad17bffba31574948
Maksim.S <maxsic85@mail.ru>
2022-06-02 02:56:40 +0300

    crerate OutlineSelector.cs

---
+++ Strategy/Assets/Scripts/UserControlSystem/UI/View/OutlineSelector.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using UnityEngine;
+
+public class OutlineSelector : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer[] _renderers;
+    [SerializeField] private Material _outlineMaterial;
+    private bool _isSelectedCache;
+    public void SetSelected(bool isSelected)
+    {
+        if (isSelected == _isSelectedCache)
+        {
+            return;
+        }
+        for (int i = 0; i < _renderers.Length; i++)
+        {
+            var renderer = _renderers[i];
+            var materialsList = renderer.materials.ToList();
+            if (isSelected)
+            {
+                materialsList.Add(_outlineMaterial);
+            }
+            else
+            {
+                materialsList.RemoveAt(materialsList.Count - 1);
+            }
+            renderer.materials = materialsList.ToArray();
+        }
+        _isSelectedCache = isSelected;
+    }
+}

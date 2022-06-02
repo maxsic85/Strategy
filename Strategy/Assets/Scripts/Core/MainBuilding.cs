9d1f294b7b66dada0bb215358c12fe5b4acb7adf
Maksim.S <maxsic85@mail.ru>
2022-06-02 15:24:55 +0300

    add CommandButtonView

--- Strategy/Assets/Scripts/Core/MainBuilding.cs
+++ Strategy/Assets/Scripts/Core/MainBuilding.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace Core
 {
-    public sealed class MainBuilding : CommandExecutorBase<IProduceUnitCommand>, IUnitProducer, ISelectable
+    public sealed class MainBuilding : CommandExecutorBase<IProduceUnitCommand>,  ISelectable
     {
         public float Health => _health;
         public float MaxHealth => _maxHealth;
---
+++ Strategy/Assets/Scripts/UserControlSystem/UI/View/CommandButtonsView.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class CommandButtonsView : MonoBehaviour
+{
+    public Action<ICommandExecutor> OnClick;
+    [SerializeField] private GameObject _attackButton;
+    [SerializeField] private GameObject _moveButton;
+    [SerializeField] private GameObject _patrolButton;
+    [SerializeField] private GameObject _stopButton;
+    [SerializeField] private GameObject _produceUnitButton;
+    private Dictionary<Type, GameObject> _buttonsByExecutorType;
+    private void Start()
+    {
+        _buttonsByExecutorType = new Dictionary<Type, GameObject>();
+        _buttonsByExecutorType
+        .Add(typeof(CommandExecutorBase<IAttackCommand>), _attackButton);
+        _buttonsByExecutorType
+        .Add(typeof(CommandExecutorBase<IMoveCommand>), _moveButton);
+        _buttonsByExecutorType
+        .Add(typeof(CommandExecutorBase<IPatrolCommand>), _patrolButton);
+        _buttonsByExecutorType
+        .Add(typeof(CommandExecutorBase<IStopCommand>), _stopButton);
+        _buttonsByExecutorType
+        .Add(typeof(CommandExecutorBase<IProduceUnitCommand>),
+        _produceUnitButton);
+    }
+    public void MakeLayout(IEnumerable<ICommandExecutor> commandExecutors)
+    {
+        foreach (var currentExecutor in commandExecutors)
+        {
+            var buttonGameObject = _buttonsByExecutorType
+            .Where(type => type
+            .Key
+            .IsAssignableFrom(currentExecutor.GetType())
+            )
+            .First()
+            .Value;
+            buttonGameObject.SetActive(true);
+            var button = buttonGameObject.GetComponent<Button>();
+            button.onClick.AddListener(() => OnClick?.Invoke(currentExecutor));
+        }
+    }
+    public void Clear()
+    {
+        foreach (var kvp in _buttonsByExecutorType)
+        {
+            kvp.Value.GetComponent<Button>().onClick.RemoveAllListeners();
+            kvp.Value.SetActive(false);
+        }
+    }
+}

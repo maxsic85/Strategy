a606adaab43d155c6c831e245773e358eb2f325f
Maksim.S <maxsic85@mail.ru>
2022-06-02 21:41:24 +0300

    edit AssetsInjector

--- Strategy/Assets/Scripts/Utils/AssetsInjector.cs
+++ Strategy/Assets/Scripts/Utils/AssetsInjector.cs
@@ -1,7 +1,26 @@
-﻿public static class AssetsInjector
+﻿using System;
+using System.Reflection;
+public static class AssetsInjector
 {
+    private static readonly Type _injectAssetAttributeType = typeof(InjectAssetAttribute);
     public static T Inject<T>(this AssetsContext context, T target)
     {
+        var targetType = target.GetType();
+        var allFields = targetType.GetFields(BindingFlags.NonPublic | BindingFlags.Public
+        | BindingFlags.Instance);
+        for (int i = 0; i < allFields.Length; i++)
+        {
+            var fieldInfo = allFields[i];
+            var injectAssetAttribute =
+            fieldInfo.GetCustomAttribute(_injectAssetAttributeType) as InjectAssetAttribute;
+            if (injectAssetAttribute == null)
+            {
+                continue;
+            }
+            var objectToInject = context.GetObjectOfType(fieldInfo.FieldType,
+            injectAssetAttribute.AssetName);
+            fieldInfo.SetValue(target, objectToInject);
+        }
         return target;
     }
 }

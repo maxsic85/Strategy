36f6d038124c82d7d078eb15ade33c54f35439e7
Maksim.S <maxsic85@mail.ru>
2022-06-02 22:03:03 +0300

    add MainUnit.cs

--- Strategy/Assets/Scripts/Abstractions/IMoveCommand.cs
+++ Strategy/Assets/Scripts/Abstractions/IMoveCommand.cs
@@ -1,2 +1,6 @@
-﻿public interface IMoveCommand : ICommand
-{ }
+﻿using UnityEngine;
+
+public interface IMoveCommand : ICommand
+{
+   
+}
---
+++ Strategy/Assets/Scripts/Core/MainUnit.cs
@@ -0,0 +1,26 @@
+using Abstractions;
+using UnityEngine;
+
+namespace Core
+{
+    public sealed class MainUnit : CommandExecutorBase<IMoveCommand>, ISelectable
+    {
+        public float Health => _health;
+        public float MaxHealth => _maxHealth;
+        public Sprite Icon => _icon;
+
+        [SerializeField] private Transform _unitsParent;
+
+        [SerializeField] private float _maxHealth = 1000;
+        [SerializeField] private Sprite _icon;
+
+        private float _health = 1000;
+
+        public override void ExecuteSpecificCommand(IMoveCommand command)
+        {
+            Debug.Log($"Moving {gameObject.name}");
+        }
+
+
+    }
+}

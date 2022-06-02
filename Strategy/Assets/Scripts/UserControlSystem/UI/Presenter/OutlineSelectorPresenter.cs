4638d898525e91acfb4cf0c7a45d4f329656e37a
Maksim.S <maxsic85@mail.ru>
2022-06-02 03:02:17 +0300

    create OutlineSelectionPresenter

---
+++ Strategy/Assets/Scripts/UserControlSystem/UI/Presenter/OutlineSelectorPresenter.cs
@@ -0,0 +1,42 @@
+using Abstractions;
+using UnityEngine;
+using UserControlSystem;
+
+public class OutlineSelectorPresenter : MonoBehaviour
+{
+    [SerializeField] private SelectableValue _selectable;
+    private OutlineSelector[] _outlineSelectors;
+    private ISelectable _currentSelectable;
+    private void Start()
+    {
+        _selectable.OnSelected += onSelected;
+        onSelected(_selectable.CurrentValue);
+    }
+    private void onSelected(ISelectable selectable)
+    {
+        if (_currentSelectable == selectable)
+        {
+            return;
+        }
+        _currentSelectable = selectable;
+        setSelected(_outlineSelectors, false);
+        _outlineSelectors = null;
+
+        if (selectable != null)
+        {
+            _outlineSelectors = (selectable as
+            Component).GetComponentsInParent<OutlineSelector>();
+            setSelected(_outlineSelectors, true);
+        }
+        static void setSelected(OutlineSelector[] selectors, bool value)
+        {
+            if (selectors != null)
+            {
+                for (int i = 0; i < selectors.Length; i++)
+                {
+                    selectors[i].SetSelected(value);
+                }
+            }
+        }
+    }
+}

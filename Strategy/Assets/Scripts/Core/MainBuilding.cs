5a176859916cf8f165f43aa3842f10ebb580abd9
Maksim.S <maxsic85@mail.ru>
2022-06-02 15:07:44 +0300

    edit MainBuilding

--- Strategy/Assets/Scripts/Core/MainBuilding.cs
+++ Strategy/Assets/Scripts/Core/MainBuilding.cs
@@ -3,13 +3,12 @@ using UnityEngine;
 
 namespace Core
 {
-    public sealed class MainBuilding : MonoBehaviour, IUnitProducer, ISelectable
+    public sealed class MainBuilding : CommandExecutorBase<IProduceUnitCommand>, IUnitProducer, ISelectable
     {
         public float Health => _health;
         public float MaxHealth => _maxHealth;
         public Sprite Icon => _icon;
 
-        [SerializeField] private GameObject _unitPrefab;
         [SerializeField] private Transform _unitsParent;
 
         [SerializeField] private float _maxHealth = 1000;
@@ -17,12 +16,10 @@ namespace Core
 
         private float _health = 1000;
 
-        public void ProduceUnit()
+        public override void ExecuteSpecificCommand(IProduceUnitCommand command)
         {
-            Instantiate(_unitPrefab,
-                new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)),
-                Quaternion.identity,
-                _unitsParent);
+            Instantiate(command.UnitPrefab, new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)), Quaternion.identity, _unitsParent);
         }
+
     }
 }

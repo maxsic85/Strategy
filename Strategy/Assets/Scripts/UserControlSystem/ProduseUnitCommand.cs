66ded1bdb49912d897dbecb1c5b44a1f3ef7f794
Maksim.S <maxsic85@mail.ru>
2022-06-02 16:15:09 +0300

    edit ProduceUnitCommand

--- Strategy/Assets/Scripts/UserControlSystem/ProduseUnitCommand.cs
+++ Strategy/Assets/Scripts/UserControlSystem/ProduseUnitCommand.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 public class ProduceUnitCommand : IProduceUnitCommand
 {
     public GameObject UnitPrefab => _unitPrefab;
-    [SerializeField] private GameObject _unitPrefab;
+    public GameObject _unitPrefab;
+
+    public ProduceUnitCommand(GameObject unitPrefab)
+    {
+        _unitPrefab = unitPrefab;
+    }
 }

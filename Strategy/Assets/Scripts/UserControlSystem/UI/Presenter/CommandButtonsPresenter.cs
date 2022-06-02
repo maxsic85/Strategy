a08174f96eaedaf1128738f9ab8782b3bffafa8c
Maksim.S <maxsic85@mail.ru>
2022-06-02 15:59:36 +0300

    add CommandButtonsPresenter

---
+++ Strategy/Assets/Scripts/UserControlSystem/UI/Presenter/CommandButtonsPresenter.cs
@@ -0,0 +1,45 @@
+﻿using Abstractions;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UserControlSystem;
+
+public class CommandButtonsPresenter : MonoBehaviour
+{
+    [SerializeField] private SelectableValue _selectable;
+    [SerializeField] private CommandButtonsView _view;
+    private ISelectable _currentSelectable;
+    private void Start()
+    {
+        _selectable.OnSelected += onSelected;
+        onSelected(_selectable.CurrentValue);
+        _view.OnClick += onButtonClick;
+    }
+    private void onSelected(ISelectable selectable)
+    {
+        if (_currentSelectable == selectable)
+        {
+            return;
+        }
+        _currentSelectable = selectable;
+        _view.Clear();
+        if (selectable != null)
+        {
+            var commandExecutors = new List<ICommandExecutor>();
+            commandExecutors.AddRange((selectable as
+            Component).GetComponentsInParent<ICommandExecutor>());
+            _view.MakeLayout(commandExecutors);
+        }
+    }
+    private void onButtonClick(ICommandExecutor commandExecutor)
+    {
+        var unitProducer = commandExecutor as
+        CommandExecutorBase<IProduceUnitCommand>;
+        if (unitProducer != null)
+        {
+            unitProducer.ExecuteSpecificCommand(new ProduceUnitCommand());
+            return;
+        }
+        throw new   ApplicationException($"{nameof(CommandButtonsPresenter)}.{nameof(onButtonClick)}: Unknown type of commands executor: { commandExecutor.GetType().FullName }!");
+    }
+}
--- Strategy/Assets/Scripts/UserControlSystem/UI/View/CommandButtonsView.cs
+++ Strategy/Assets/Scripts/UserControlSystem/UI/View/CommandButtonsView.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CommandButtonsView : MonoBehaviour
 {

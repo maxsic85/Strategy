12e4058385cded6e4606253a968244bbf31559af
Maksim.S <maxsic85@mail.ru>
2022-06-06 02:48:04 +0300

    edit presenter and CommandButtonsView

--- Strategy/Assets/Scripts/UserControlSystem/UI/Presenter/CommandButtonsPresenter.cs
+++ Strategy/Assets/Scripts/UserControlSystem/UI/Presenter/CommandButtonsPresenter.cs
@@ -3,18 +3,26 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UserControlSystem;
+using Zenject;
 
 public class CommandButtonsPresenter : MonoBehaviour
 {
     [SerializeField] private SelectableValue _selectable;
     [SerializeField] private CommandButtonsView _view;
-    [SerializeField] private AssetsContext _context;
     private ISelectable _currentSelectable;
+
+    [Inject] private CommandButtonsModel _buttonModel;
+
     private void Start()
     {
+        _view.OnClick += _buttonModel.OnCommandButtonClicked;
+        _buttonModel.OnCommandSent += _view.UnblockAllInteractions;
+        _buttonModel.OnCommandCancel += _view.UnblockAllInteractions;
+        _buttonModel.OnCommandAccepted += _view.BlockInteractions;
+
+
         _selectable.OnSelected += onSelected;
         onSelected(_selectable.CurrentValue);
-        _view.OnClick += onButtonClick;
     }
     private void onSelected(ISelectable selectable)
     {
@@ -32,86 +40,86 @@ public class CommandButtonsPresenter : MonoBehaviour
             _view.MakeLayout(commandExecutors);
         }
     }
-    private void onButtonClick(ICommandExecutor commandExecutor)
-    {
+    //private void onButtonClick(ICommandExecutor commandExecutor)
+    //{
 
-        if (commandExecutor is null)
-        {
-            throw new ApplicationException($"{nameof(CommandButtonsPresenter)}.{nameof(onButtonClick)}: Unknown type of commands executor: { commandExecutor.GetType().FullName }!");
-        }
-        if (commandExecutor is CommandExecutorBase<IMoveCommand> moveExecuter)
-        {
-            LetsMove(moveExecuter);
-            return;
+    //    if (commandExecutor is null)
+    //    {
+    //        throw new ApplicationException($"{nameof(CommandButtonsPresenter)}.{nameof(onButtonClick)}: Unknown type of commands executor: { commandExecutor.GetType().FullName }!");
+    //    }
+    //    if (commandExecutor is CommandExecutorBase<IMoveCommand> moveExecuter)
+    //    {
+    //        LetsMove(moveExecuter);
+    //        return;
 
-        }
-        if (commandExecutor is CommandExecutorBase<IProduceUnitCommand> produseExecuter)
-        {
-            LetsProduse(produseExecuter);
-            return;
-        }
-        if (commandExecutor is CommandExecutorBase<IAttackCommand> atasckExecuter)
-        {
-            LetsAttack(atasckExecuter);
-            return;
-        }
-        if (commandExecutor is CommandExecutorBase<IPatrolCommand> patrolExecurter)
-        {
-            LetsPatrol(patrolExecurter);
-            return;
-        }
-        if (commandExecutor is CommandExecutorBase<IStopCommand> stopExecurter)
-        {
-            LetsStop(stopExecurter);
-            return;
-        }
+    //    }
+    //    if (commandExecutor is CommandExecutorBase<IProduceUnitCommand> produseExecuter)
+    //    {
+    //        LetsProduse(produseExecuter);
+    //        return;
+    //    }
+    //    if (commandExecutor is CommandExecutorBase<IAttackCommand> atasckExecuter)
+    //    {
+    //        LetsAttack(atasckExecuter);
+    //        return;
+    //    }
+    //    if (commandExecutor is CommandExecutorBase<IPatrolCommand> patrolExecurter)
+    //    {
+    //        LetsPatrol(patrolExecurter);
+    //        return;
+    //    }
+    //    if (commandExecutor is CommandExecutorBase<IStopCommand> stopExecurter)
+    //    {
+    //        LetsStop(stopExecurter);
+    //        return;
+    //    }
 
 
-        void LetsMove(CommandExecutorBase<IMoveCommand> commandExecutor)
-        {
-            if (commandExecutor != null)
-            {
-                commandExecutor.ExecuteSpecificCommand(_context.Inject(new MoveCommand()));
-                return;
-            }
-        }
+    //    void LetsMove(CommandExecutorBase<IMoveCommand> commandExecutor)
+    //    {
+    //        if (commandExecutor != null)
+    //        {
+    //            commandExecutor.ExecuteSpecificCommand(_context.Inject(new MoveCommand()));
+    //            return;
+    //        }
+    //    }
 
-        void LetsProduse(CommandExecutorBase<IProduceUnitCommand> commandExecutor)
-        {
-            if (commandExecutor != null)
-            {
-                //  IProduceUnitCommand  a= (IProduceUnitCommand)new ProduceUnitCommandHeir().GetType().BaseType.GetInterface(nameof(IProduceUnitCommand));
-                //  var  a=new ProduceUnitCommandHeir().GetType().BaseType;
+    //    void LetsProduse(CommandExecutorBase<IProduceUnitCommand> commandExecutor)
+    //    {
+    //        if (commandExecutor != null)
+    //        {
+    //            //  IProduceUnitCommand  a= (IProduceUnitCommand)new ProduceUnitCommandHeir().GetType().BaseType.GetInterface(nameof(IProduceUnitCommand));
+    //            //  var  a=new ProduceUnitCommandHeir().GetType().BaseType;
 
-                commandExecutor.ExecuteSpecificCommand(_context.Inject(new ProduceUnitCommandHeir()));
-                return;
-            }
-        }
+    //            commandExecutor.ExecuteSpecificCommand(_context.Inject(new ProduceUnitCommandHeir()));
+    //            return;
+    //        }
+    //    }
 
-        void LetsAttack(CommandExecutorBase<IAttackCommand> commandExecutor)
-        {
-            if (commandExecutor != null)
-            {
-                commandExecutor.ExecuteSpecificCommand(_context.Inject(new AttackCommand()));
-                return;
-            }
-        }
-        void LetsPatrol(CommandExecutorBase<IPatrolCommand> commandExecutor)
-        {
-            if (commandExecutor != null)
-            {
-                commandExecutor.ExecuteSpecificCommand(_context.Inject(new PatrolCommand()));
-                return;
-            }
-        }
+    //    void LetsAttack(CommandExecutorBase<IAttackCommand> commandExecutor)
+    //    {
+    //        if (commandExecutor != null)
+    //        {
+    //            commandExecutor.ExecuteSpecificCommand(_context.Inject(new AttackCommand()));
+    //            return;
+    //        }
+    //    }
+    //    void LetsPatrol(CommandExecutorBase<IPatrolCommand> commandExecutor)
+    //    {
+    //        if (commandExecutor != null)
+    //        {
+    //            commandExecutor.ExecuteSpecificCommand(_context.Inject(new PatrolCommand()));
+    //            return;
+    //        }
+    //    }
 
-        void LetsStop(CommandExecutorBase<IStopCommand> commandExecutor)
-        {
-            if (commandExecutor != null)
-            {
-                commandExecutor.ExecuteSpecificCommand(_context.Inject(new StopCommand()));
-                return;
-            }
-        }
-    }
+    //    void LetsStop(CommandExecutorBase<IStopCommand> commandExecutor)
+    //    {
+    //        if (commandExecutor != null)
+    //        {
+    //            commandExecutor.ExecuteSpecificCommand(_context.Inject(new StopCommand()));
+    //            return;
+    //        }
+    //    }
+    //}
 }
--- Strategy/Assets/Scripts/UserControlSystem/UI/View/CommandButtonsView.cs
+++ Strategy/Assets/Scripts/UserControlSystem/UI/View/CommandButtonsView.cs
@@ -28,36 +28,48 @@ public class CommandButtonsView : MonoBehaviour
         .Add(typeof(CommandExecutorBase<IProduceUnitCommand>),
         _produceUnitButton);
     }
+    public void BlockInteractions(ICommandExecutor ce)
+    {
+        UnblockAllInteractions();
+        getButtonGameObjectByType(ce.GetType())
+        .GetComponent<Selectable>().interactable = false;
+    }
+    public void UnblockAllInteractions() => setInteractible(true);
+    private void setInteractible(bool value)
+    {
+        _attackButton.GetComponent<Selectable>().interactable = value;
+        _moveButton.GetComponent<Selectable>().interactable = value;
+        _patrolButton.GetComponent<Selectable>().interactable = value;
+        _stopButton.GetComponent<Selectable>().interactable = value;
+        _produceUnitButton.GetComponent<Selectable>().interactable =
+        value;
+    }
     public void MakeLayout(IEnumerable<ICommandExecutor> commandExecutors)
     {
         foreach (var currentExecutor in commandExecutors)
         {
-            var buttonGameObject = _buttonsByExecutorType
-            .Where(type => type
-            .Key
-            .IsAssignableFrom(currentExecutor.GetType())
-            )
-            .FirstOrDefault()
-            .Value;
-            if (buttonGameObject != null)
-            {
+            var buttonGameObject =
+            getButtonGameObjectByType(currentExecutor.GetType());
             buttonGameObject.SetActive(true);
             var button = buttonGameObject.GetComponent<Button>();
-            button.onClick.AddListener(() => OnClick?.Invoke(currentExecutor));
-           
-            }
-            else
-            {
-               print( _buttonsByExecutorType.FirstOrDefault().Value);
-            }
-               
+            button.onClick.AddListener(() =>
+            OnClick?.Invoke(currentExecutor));
         }
+    }
+    private GameObject getButtonGameObjectByType(Type executorInstanceType)
+    {
+        return _buttonsByExecutorType
+        .Where(type =>
+        type.Key.IsAssignableFrom(executorInstanceType))
+        .First()
+        .Value;
     }
     public void Clear()
     {
         foreach (var kvp in _buttonsByExecutorType)
         {
-            kvp.Value.GetComponent<Button>().onClick.RemoveAllListeners();
+            kvp.Value
+            .GetComponent<Button>().onClick.RemoveAllListeners();
             kvp.Value.SetActive(false);
         }
     }

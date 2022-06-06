d685445dea73f0f1a8c0f18bb1963e72772cfb93
Maksim.S <maxsic85@mail.ru>
2022-06-06 18:29:29 +0300

    _unitPrefab changed to private in  ProduceUnitCommand

--- Strategy/Assets/Scripts/UserControlSystem/ProduseUnitCommand.cs
+++ Strategy/Assets/Scripts/UserControlSystem/ProduseUnitCommand.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public partial  class  ProduceUnitCommand : IProduceUnitCommand
 {
     public GameObject UnitPrefab => _unitPrefab;
-    [InjectAsset("Chomper")] public GameObject _unitPrefab;
+    [InjectAsset("Chomper")] private GameObject _unitPrefab;
 
 
 }

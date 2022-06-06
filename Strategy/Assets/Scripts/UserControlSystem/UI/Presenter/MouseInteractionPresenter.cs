13e40c1a41e0c7ebde968a8e0a9bfb7fe09772b1
Maksim.S <maxsic85@mail.ru>
2022-06-06 18:56:30 +0300

    edit MouseInteractionPresenter

--- Strategy/Assets/Scripts/UserControlSystem/UI/Presenter/MouseInteractionPresenter.cs
+++ Strategy/Assets/Scripts/UserControlSystem/UI/Presenter/MouseInteractionPresenter.cs
@@ -9,23 +9,49 @@ public sealed class MouseInteractionPresenter : MonoBehaviour
     [SerializeField] private Camera _camera;
     [SerializeField] private SelectableValue _selectedObject;
     [SerializeField] private EventSystem _eventSystem;
+
+    [SerializeField] private Vector3Value _groundClicksRMB;
+    [SerializeField] private Transform _groundTransform;
+
+    private Plane _groundPlane;
+
+    private void Start()
+    {
+        _groundPlane = new Plane(_groundTransform.up, 0);
+    }
     private void Update()
     {
-        if (!Input.GetMouseButtonUp(0))
+        if (!Input.GetMouseButtonUp(0) && !Input.GetMouseButton(1))
         {
             return;
         }
-        var hits = Physics.RaycastAll(_camera.ScreenPointToRay(Input.mousePosition));
-        if (hits.Length == 0)
+        if (_eventSystem.IsPointerOverGameObject())
         {
             return;
         }
-        if (!_eventSystem.IsPointerOverGameObject())
-        { 
+        var ray = _camera.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonUp(0))
+        {
+            var hits = Physics.RaycastAll(ray);
+            if (hits.Length == 0)
+            {
+                return;
+            }
             var selectable = hits
-            .Select(hit => hit.collider.GetComponentInParent<ISelectable>())
-            .FirstOrDefault(c => c != null);
-        _selectedObject.SetValue(selectable);
+            .Select(hit =>
+            hit.collider.GetComponentInParent<ISelectable>())
+            .Where(c => c != null)
+            .FirstOrDefault();
+            _selectedObject.SetValue(selectable);
+        }
+        else
+        {
+            if (_groundPlane.Raycast(ray, out var enter))
+            {
+                _groundClicksRMB.SetValue(ray.origin + ray.direction
+                * enter);
+            }
         }
     }
+
 }

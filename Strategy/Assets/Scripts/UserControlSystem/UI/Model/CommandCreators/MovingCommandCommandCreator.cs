656597720be17e42a9e6aa7dcfb57f9cb050ca4a
Maksim.S <maxsic85@mail.ru>
2022-06-06 18:59:12 +0300

    edit MovingCommandCommandCreator

--- Strategy/Assets/Scripts/UserControlSystem/UI/Model/CommandCreators/MovingCommandCommandCreator.cs
+++ Strategy/Assets/Scripts/UserControlSystem/UI/Model/CommandCreators/MovingCommandCommandCreator.cs
@@ -5,8 +5,27 @@ using UnityEngine;
 public class MovingCommandCommandCreator : CommandCreatorBase<IMoveCommand>
 {
     [Inject] private AssetsContext _context;
+    private Action<IMoveCommand> _creationCallback;
+
+    [Inject]
+    private void Init(Vector3Value groundClicks)
+    {
+        groundClicks.OnNewValue += onNewValue;
+    }
+
+    private void onNewValue(Vector3 groundClick)
+    {
+        _creationCallback?.Invoke(_context.Inject(new MoveCommand(groundClick)));
+        _creationCallback = null;
+    }
     protected override void classSpecificCommandCreation(Action<IMoveCommand> creationCallback)
     {
-        creationCallback?.Invoke(_context.Inject(new MoveCommand(Vector3.zero)));
+        _creationCallback = creationCallback;
     }
+    public override void ProcessCancel()
+    {
+        base.ProcessCancel();
+        _creationCallback = null;
+    }
+
 }

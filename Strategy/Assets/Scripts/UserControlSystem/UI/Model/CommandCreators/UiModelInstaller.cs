52996ed60f44132a035c6c88cf7411baa216a3c6
Maksim.S <maxsic85@mail.ru>
2022-06-06 19:07:22 +0300

    Update UiModelInstaller.cs

--- Strategy/Assets/Scripts/UserControlSystem/UI/Model/CommandCreators/UiModelInstaller.cs
+++ Strategy/Assets/Scripts/UserControlSystem/UI/Model/CommandCreators/UiModelInstaller.cs
@@ -3,9 +3,13 @@ using Zenject;
 public class UiModelInstaller : MonoInstaller
 {
     [SerializeField] private AssetsContext _legacyContext;
+    [SerializeField] private Vector3Value _vector3Value;
+
     public override void InstallBindings()
     {
         Container.Bind<AssetsContext>().FromInstance(_legacyContext);
+        Container.Bind<Vector3Value>().FromInstance(_vector3Value);
+
         Container.Bind<CommandCreatorBase<IProduceUnitCommand>>()
         .To<ProduceUnitCommandCommandCreator>().AsTransient();
         Container.Bind<CommandCreatorBase<IAttackCommand>>()

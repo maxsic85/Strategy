a864c6faf579cf7e868e5c505fb35cd285c073b9
Maksim.S <maxsic85@mail.ru>
2022-06-10 02:21:58 +0300

    create AsyncExtensions

---
+++ Strategy/Assets/Scripts/Utils/AsyncExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+public static class AsyncExtensions
+{
+    public struct Void { }
+    public static async Task<TResult> WithCancellation<TResult>(this Task<TResult>
+    originalTask, CancellationToken ct)
+    {
+        var cancelTask = new TaskCompletionSource<Void>();
+        using (ct.Register(t => ((TaskCompletionSource<Void>)t).TrySetResult(new
+        Void()), cancelTask))
+        {
+            var any = await Task.WhenAny(originalTask, cancelTask.Task);
+            if (any == cancelTask.Task)
+            {
+                ct.ThrowIfCancellationRequested();
+            }
+        }
+        return await originalTask;
+    }
+}

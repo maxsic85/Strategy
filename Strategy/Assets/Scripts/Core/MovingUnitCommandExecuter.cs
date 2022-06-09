a2666fbf787cc1f5565847a27048f7eabe8cc8fa
Maksim.S <maxsic85@mail.ru>
2022-06-10 02:53:30 +0300

    edit MovingUnitCommandExecuter and add nawmesh and bake static obj-s

--- Strategy/Assets/Scripts/Core/MovingUnitCommandExecuter.cs
+++ Strategy/Assets/Scripts/Core/MovingUnitCommandExecuter.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class MovingUnitCommandExecuter : CommandExecutorBase<IMoveCommand>
 {
     public override void ExecuteSpecificCommand(IMoveCommand command)
     {
         Debug.Log($"{name} is moving to {command.Target}!");
+        GetComponent<NavMeshAgent>().destination = command.Target;
     }
 }

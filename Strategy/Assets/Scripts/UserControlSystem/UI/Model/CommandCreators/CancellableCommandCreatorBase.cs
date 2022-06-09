a511cdb7e6e0845237f5116c39525f020268defc
Maksim.S <maxsic85@mail.ru>
2022-06-10 02:26:53 +0300

    create CancellableCommandCreatorBase

---
+++ Strategy/Assets/Scripts/UserControlSystem/UI/Model/CommandCreators/CancellableCommandCreatorBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using Zenject;
+
+public abstract class CancellableCommandCreatorBase<TCommand, TArgument> :
+CommandCreatorBase<TCommand> where TCommand : ICommand
+{
+    [Inject] private AssetsContext _context;
+    [Inject] private IAwaitable<TArgument> _awaitableArgument;
+    private CancellationTokenSource _ctSource;
+    protected override sealed async void
+    classSpecificCommandCreation(Action<TCommand> creationCallback)
+    {
+        _ctSource = new CancellationTokenSource();
+        try
+        {
+            var argument = await _awaitableArgument.WithCancellation(_ctSource.Token);
+            creationCallback?
+            .Invoke(_context.Inject(createCommand(argument)));
+        }
+        catch { }
+    }
+    protected abstract TCommand createCommand(TArgument argument);
+    public override void ProcessCancel()
+    {
+        base.ProcessCancel();
+        if (_ctSource != null)
+        {
+            _ctSource.Cancel();
+            _ctSource.Dispose();
+            _ctSource = null;
+        }
+    }
+}
--- Strategy/Assets/Scripts/UserControlSystem/UI/Model/CommandCreators/CommandCreatorBase.cs
+++ Strategy/Assets/Scripts/UserControlSystem/UI/Model/CommandCreators/CommandCreatorBase.cs
@@ -16,4 +16,3 @@ public abstract class CommandCreatorBase<T> where T : ICommand
     creationCallback);
     public virtual void ProcessCancel() { }
 }
-

5c4cf89499c83ae23d4419274bcd0084f9e6fed1
Maksim.S <maxsic85@mail.ru>
2022-06-10 02:41:27 +0300

    Update AttakcingCommandCommandCreator.cs

--- Strategy/Assets/Scripts/UserControlSystem/UI/Model/CommandCreators/AttakcingCommandCommandCreator.cs
+++ Strategy/Assets/Scripts/UserControlSystem/UI/Model/CommandCreators/AttakcingCommandCommandCreator.cs
@@ -1,36 +1,5 @@
-﻿using System;
-using Zenject;
-using Abstractions;
-using System.Threading;
-
-public class AttakcingCommandCommandCreator : CommandCreatorBase<IAttackCommand>
-
+﻿public class AttakcingCommandCommandCreator : CancellableCommandCreatorBase<IAttackCommand, IAttackable>
 {
-    [Inject] private AssetsContext _context;
-    [Inject] private AtackValue _atackValue;
-
-    private CancellationTokenSource _ctSource;
-
-    protected override async void classSpecificCommandCreation(Action<IAttackCommand> creationCallback)
-    {
-        _ctSource = new CancellationTokenSource();
-        try
-        {
-            var attackable = await _atackValue.WithCancellation(_ctSource.Token);
-            creationCallback?.Invoke(_context.Inject(new AttackCommand(attackable)));
-        }
-        catch { }
-    }
-    public override void ProcessCancel()
-    {
-        base.ProcessCancel();
-        if (_ctSource != null)
-        {
-            _ctSource.Cancel();
-            _ctSource.Dispose();
-            _ctSource = null;
-        }
-    }
-
+    protected override IAttackCommand createCommand(IAttackable argument) => new AttackCommand(argument);
 
 }

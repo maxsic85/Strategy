6fed43e8650507e91ae8c3517606261a790c8f70
Maksim.S <maxsic85@mail.ru>
2022-06-10 03:31:30 +0300

    add animator

--- Strategy/Assets/Scripts/Core/MovingUnitCommandExecuter.cs
+++ Strategy/Assets/Scripts/Core/MovingUnitCommandExecuter.cs
@@ -3,9 +3,17 @@ using UnityEngine.AI;
 
 public class MovingUnitCommandExecuter : CommandExecutorBase<IMoveCommand>
 {
+    [SerializeField] private UnitMovementStop _stop;
+    [SerializeField] private Animator _animator;
+
+
     public override void ExecuteSpecificCommand(IMoveCommand command)
     {
         Debug.Log($"{name} is moving to {command.Target}!");
         GetComponent<NavMeshAgent>().destination = command.Target;
+        _animator.SetTrigger("Walk");
+        await _stop;
+        _animator.SetTrigger("Idle");
+
     }
 }
---
+++ Strategy/Assets/Scripts/Core/UnitMovementStop.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class UnitMovementStop : MonoBehaviour, IAwaitable<AsyncExtensions.Void>
+{
+    public class StopAwaiter : IAwaiter<AsyncExtensions.Void>
+    {
+        private readonly UnitMovementStop _unitMovementStop;
+        private Action _continuation;
+        private bool _isCompleted;
+        public StopAwaiter(UnitMovementStop unitMovementStop)
+        {
+            _unitMovementStop = unitMovementStop;
+            _unitMovementStop.OnStop += onStop;
+        }
+        private void onStop()
+        {
+            _unitMovementStop.OnStop -= onStop;
+            _isCompleted = true;
+            _continuation?.Invoke();
+        }
+        public void OnCompleted(Action continuation)
+        {
+            if (_isCompleted)
+            {
+                continuation?.Invoke();
+            }
+            else
+            {
+                _continuation = continuation;
+            }
+        }
+        public bool IsCompleted => _isCompleted;
+        public AsyncExtensions.Void GetResult() => new AsyncExtensions.Void();
+    }
+    public event Action OnStop;
+    [SerializeField] private NavMeshAgent _agent;
+    void Update()
+    {
+        if (!_agent.pathPending)
+        {
+            if (_agent.remainingDistance <= _agent.stoppingDistance)
+            {
+                if (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f)
+                {
+                    OnStop?.Invoke();
+                }
+            }
+        }
+    }
+    public IAwaiter<AsyncExtensions.Void> GetAwaiter() => new StopAwaiter(this);
+}

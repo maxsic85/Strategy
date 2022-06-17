e8ed6dfa5049fdeab11d58af02ff3f9cd748fedc
Maksim.S <maxsic85@mail.ru>
2022-06-17 03:43:33 +0300

    add opPanelPresenter

---
+++ Strategy/Assets/Scripts/UserControlSystem/UI/Presenter/TopPanelPresenter.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+using UniRx;
+using System;
+public class TopPanelPresenter : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField _inputField;
+    [Inject]
+    private void Init(ITimeModel timeModel)
+    {
+        timeModel.GameTime.Subscribe(seconds =>
+        {
+            var t = TimeSpan.FromSeconds(seconds);
+            _inputField.text = string.Format("{0:D2}:{1:D2}",
+            t.Minutes,
+            t.Seconds);
+        });
+    }
+}

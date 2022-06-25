272af7dfc3e1616ee9a277cab7bc611879bbb01a
Maksim.S <maxsic85@mail.ru>
2022-06-25 19:39:45 +0300

    Update IProduceUnitCommand.cs

--- Strategy/Assets/Scripts/Abstractions/IProduceUnitCommand.cs
+++ Strategy/Assets/Scripts/Abstractions/IProduceUnitCommand.cs
@@ -1,9 +1,8 @@
 ﻿using UnityEngine;
 
-public interface IProduceUnitCommand : ICommand
+public interface IProduceUnitCommand : ICommand, IIconHolder
 {
     float ProductionTime { get; }
-    Sprite Icon { get; }
     GameObject UnitPrefab { get; }
     string UnitName { get; }
     

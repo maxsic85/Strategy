eea363e77f2a0f1f353bbe58099ddaff32b7315a
Maksim.S <maxsic85@mail.ru>
2022-06-25 19:25:16 +0300

    edit IProduceUnitCommand

--- Strategy/Assets/Scripts/Abstractions/IProduceUnitCommand.cs
+++ Strategy/Assets/Scripts/Abstractions/IProduceUnitCommand.cs
@@ -2,5 +2,9 @@
 
 public interface IProduceUnitCommand : ICommand
 {
+    float ProductionTime { get; }
+    Sprite Icon { get; }
     GameObject UnitPrefab { get; }
+    string UnitName { get; }
+    
 }
--- Strategy/Assets/Scripts/UserControlSystem/ProduseUnitCommand.cs
+++ Strategy/Assets/Scripts/UserControlSystem/ProduseUnitCommand.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using Zenject;
 
 public partial  class  ProduceUnitCommand : IProduceUnitCommand
 {
+
+    [Inject(Id = "Chomper")] public string UnitName { get; }
+    [Inject(Id = "Chomper")] public Sprite Icon { get; }
+    [Inject(Id = "Chomper")] public float ProductionTime { get; }
+
     public GameObject UnitPrefab => _unitPrefab;
     [InjectAsset("Chomper")] private GameObject _unitPrefab;
 }

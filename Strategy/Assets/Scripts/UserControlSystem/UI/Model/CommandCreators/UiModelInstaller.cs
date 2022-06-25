adda5ec4134180931009598fe43e8f41142ee715
Maksim.S <maxsic85@mail.ru>
2022-06-25 19:30:36 +0300

    Update UiModelInstaller.cs

--- Strategy/Assets/Scripts/UserControlSystem/UI/Model/CommandCreators/UiModelInstaller.cs
+++ Strategy/Assets/Scripts/UserControlSystem/UI/Model/CommandCreators/UiModelInstaller.cs
@@ -32,5 +32,8 @@ public class UiModelInstaller : MonoInstaller
         Container.Bind<CommandCreatorBase<IStopCommand>>()
         .To<StopingCommandCommandCreator>().AsTransient();
         Container.Bind<CommandButtonsModel>().AsTransient();
+
+        Container.Bind<float>().WithId("Chomper").FromInstance(5f);
+        Container.Bind<string>().WithId("Chomper").FromInstance("Chomper");
     }
 }

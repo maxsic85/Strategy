77b4546d25061977c0aac7872fc6f0b8294cad3a
Maksim.S <maxsic85@mail.ru>
2022-06-26 19:57:51 +0300

    SetRallyPointCommandExecutor

---
+++ Strategy/Assets/Scripts/Core/CommandExecutors/SetRallyPointCommandExecutor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Abstractions;
+using Abstractions.Commands;
+using Core;
+
+public class SetRallyPointCommandExecutor :
+CommandExecutorBase<ISetRallyPointCommand>
+{
+    public override async Task ExecuteSpecificCommand(ISetRallyPointCommand
+    command)
+    {
+        GetComponent<MainUnit>().RallyPoint = command.RallyPoint;
+    }
+}
--- Strategy/Assets/Scripts/Core/MainUnit.cs
+++ Strategy/Assets/Scripts/Core/MainUnit.cs
@@ -5,10 +5,11 @@ namespace Core
 {
     public class MainUnit : MonoBehaviour ,ISelectable,IAttackable
     {
+        public Vector3 RallyPoint { get; set; }
         public float Health => _health;
         public float MaxHealth => _maxHealth;
         public Sprite Icon => _icon;
-
+        public Transform PivotPoint => _pivotPoint;
         public Transform Target => gameObject.transform;
         public Vector3 CurrenntPosition => gameObject.transform.position;
 
@@ -16,6 +17,6 @@ namespace Core
         [SerializeField] private Sprite _icon;
 
         private float _health = 100;
-    
+      [SerializeField]  private Transform _pivotPoint;
     }
 }

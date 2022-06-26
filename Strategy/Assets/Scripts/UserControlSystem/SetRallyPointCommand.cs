fb48bfb6828c69fdf89c356209ec98373d5e6699
Maksim.S <maxsic85@mail.ru>
2022-06-26 19:52:48 +0300

    SetRallyPointCommand

---
+++ Strategy/Assets/Scripts/UserControlSystem/SetRallyPointCommand.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class SetRallyPointCommand : ISetRallyPointCommand
+{
+    public Vector3 RallyPoint { get; }
+    public SetRallyPointCommand(Vector3 rallyPoint)
+    {
+        RallyPoint = rallyPoint;
+    }
+}
